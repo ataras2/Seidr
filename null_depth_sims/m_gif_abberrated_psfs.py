fixed zernike bug factor of 4 difference

null_depth_sims/m_gif_abberrated_psfs.py
@@ -8,7 +8,7 @@ import copy
 
 import matplotlib.pyplot as plt
 
-fname = "aberrated_psfs"
+fname = "aberrated_psfs_many_zern"
 data = np.load(fname + ".npz")
 
 wavefronts = data["outputs"]
@@ -16,7 +16,7 @@ non_aberrated = data["non_aberrated"]
 
 n_frames = 100
 
-Figure = plt.figure(figsize=(12, 6))
+Figure = plt.figure(figsize=(8, 4))
 
 
 # add circle to show the PL input
@@ -31,14 +31,14 @@ circle = plt.Circle(
 plt.subplot(1, 2, 1)
 amp_img = plt.imshow(np.abs(non_aberrated), cmap="inferno")
 # mark centre with a little cross
-plt.plot(non_aberrated.shape[0] // 2, non_aberrated.shape[1] // 2, "x", color="r")
+plt.plot(non_aberrated.shape[0] // 2, non_aberrated.shape[1] // 2, "+", color="r")
 plt.colorbar()
 plt.title("Amplitude")
 plt.gca().add_artist(copy.copy(circle))
 
 plt.subplot(1, 2, 2)
 phase_img = plt.imshow(np.angle(non_aberrated), cmap="twilight")
-plt.plot(non_aberrated.shape[0] // 2, non_aberrated.shape[1] // 2, "x", color="r")
+plt.plot(non_aberrated.shape[0] // 2, non_aberrated.shape[1] // 2, "+", color="r")
 plt.colorbar()
 plt.title("Phase")
 plt.gca().add_artist(copy.copy(circle))

null_depth_sims/m_grid_search_params.py
@@ -24,7 +24,7 @@ wavelength = 1.65e-6
 
 # psf params
 # input_f_number = 1.25
-input_f_number = 3
+input_f_number = 12.5
 focal_length = input_f_number * diameter
 psf_npixels = 256
 psf_pixel_scale = 2 * 2 * 10 / psf_npixels
@@ -96,8 +96,8 @@ def compute_values(core_radius, delta_n, optics):
     return n_modes, total_injection, injections[0]
 
 
-core_radii = np.linspace(3, 20, 31)
-del_n = np.linspace(1.44 - 1.4345, 1.44 - 1.40, 30)
+core_radii = np.linspace(10, 35, 10)
+del_n = np.linspace(1.44 - 1.4395, 1.44 - 1.43, 11)
 
 rr, dndn = np.meshgrid(core_radii, del_n)
 

null_depth_sims/m_make_aberrated_psfs.py
@@ -18,7 +18,7 @@ plt.rcParams["font.family"] = "serif"
 plt.rcParams["image.origin"] = "lower"
 plt.rcParams["figure.dpi"] = 72
 
-fname = "./aberrated_psfs.npz"
+fname = "./aberrated_psfs_many_zern.npz"
 
 
 # Wavefront properties
@@ -30,16 +30,25 @@ n_runs = 1_000
 wavelength = 1.65e-6
 
 # psf params
-input_f_number = 1.25
+# input_f_number = 1.25
+input_f_number = 12.5
 # input_f_number = 3
 focal_length = input_f_number * diameter
 psf_npixels = 256
-psf_pixel_scale = 2 * 2 * 5 / psf_npixels
+psf_pixel_scale = 2 * 2 * 22.5 / psf_npixels
 
 
 # aberration params:
-n_zernikes = 3  # including piston
-zernike_rms = 50e-9
+# n_zernikes = 3  # including piston
+# tip_tilt_rms = (
+#     100e-9 / 4 / np.sqrt(2)
+# )  # /4 because of dlux things, /sqrt(2) to acocunt for both tip/tilt
+
+n_zernikes = 100  # including piston
+tip_tilt_rms = (
+    50e-9 / 4 / np.sqrt(2)
+)  # /4 because of dlux things, /sqrt(2) to acocunt for both tip/tilt
+rest_rms = 5e-9 / 4
 
 
 coords = dlu.pixel_coords(wf_npixels, diameter)
@@ -49,7 +58,7 @@ circle = dlu.circle(coords, diameter / 2)
 zernike_indexes = np.arange(1, n_zernikes + 1)
 coeffs = np.zeros(zernike_indexes.shape)
 # coeffs = 300e-9 * jr.normal(jr.PRNGKey(1), zernike_indexes.shape)
-# coeffs = 50e-9 * np.array([0.0, 1.0, 0.0])
+# coeffs = tip_tilt_rms * np.array([0.0, 1.0, 0.0])
 coords = dlu.pixel_coords(wf_npixels, diameter)
 basis = dlu.zernike_basis(zernike_indexes, coords, diameter)
 
@@ -113,7 +122,16 @@ plt.imshow(np.angle(ouput_wf_complex), cmap=circular_cmap)
 plt.colorbar(label="Photons")
 plt.show()
 
-zernike_coeffs = zernike_rms * jr.normal(jr.PRNGKey(1), (n_runs, n_zernikes))
+# zernike_coeffs = zernike_rms * jr.normal(jr.PRNGKey(1), (n_runs, n_zernikes))
+
+zernike_coeffs = np.concatenate(
+    [
+        np.zeros((n_runs, 1)),
+        tip_tilt_rms * jr.normal(jr.PRNGKey(1), (n_runs, 2)),
+        rest_rms * jr.normal(jr.PRNGKey(1), (n_runs, n_zernikes - 3)),
+    ],
+    axis=1,
+)
 
 print("Making PSFs...", end="")
 outputs = jax.vmap(prop_fibre_input_field, in_axes=(None, 0))(optics, zernike_coeffs)
@@ -127,7 +145,8 @@ np.savez(
     psf_pixel_scale=psf_pixel_scale,
     psf_npixels=psf_npixels,
     input_f_number=input_f_number,
-    zernike_rms=zernike_rms,
+    tip_tilt_rms=tip_tilt_rms,
+    rest_rms=rest_rms,
     n_zernikes=n_zernikes,
     non_aberrated=ouput_wf_complex,
 )